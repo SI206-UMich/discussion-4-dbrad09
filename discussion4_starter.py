453cea686fc2768ee100d1cdc1795814771f89f1
Drew Bradley <DrewBradley@Drews-MacBook-Pro-5.local>
2023-09-20 17:50:54 -0400

    perimeter

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -51,7 +51,11 @@ class Rectangle():
     # It first verifies inputs and return "Invalid input" if they are invalid.
     # Otherwise, it returns the perimeter of the rectangle.
 
-    # YOUR CODE HERE
+    def perimeter(self):
+        if self.verify_input():
+            return 2 * (self.width + self.height)
+        else:
+            return "Invalid input"
     
 
 
